664e2381998550c4b6eb1fee1989262d5c3d0504
zoeyzding <zoeyzding@outlook.com>
2021-03-22 20:19:46 +0800

    first commit

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./BusinessList.css";
+
+import Business from "../Business/Business";
+
+class BusinessList extends Component {
+  state = {};
+  render() {
+    return (
+      <div className='BusinessList'>
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
